98b53accca3704c46797a632dcda407219eeaf37
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-11-10 15:40:28 -0500

    se agregaron mas autos ford reales

---
+++ db/migrations/2020-10-11-ford-autos.sql
@@ -0,0 +1,12 @@
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Declasse Drift Tampa V2', 'TAMPA3', '995000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2004 Mustang Cobra', 'MGT', '27000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2011 Ford Crownvic CVPI Retired', 'crownvic', '15000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2017 Ford F-150 Hennessey Velociraptor', 'velociraptor', '350000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Ford Mustang GT 2018', 'FMGT', '36000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2017 Ford Raptor', 'RAPTOR2017', '52000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2018 Ford F-350 Dually', '18f350ds', '85000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2019 Ford Mustang GT500', '19gt500', '730000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2020 Ford F-250 Super Duty', '20f250', '34000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('FORD GT 2017', 'GT2017', '450000', 'reales');
+
+
