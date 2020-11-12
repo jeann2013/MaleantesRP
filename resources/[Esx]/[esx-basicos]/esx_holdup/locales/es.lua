b0b3ab46b72cbb71274401b1bdf010d1f4d988ee
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-11-12 03:52:04 -0500

    tarduccion de robos

---
+++ resources/[Esx]/[esx-basicos]/esx_holdup/locales/es.lua
@@ -0,0 +1,19 @@
+Locales['es'] = {
+	['shop_robbery'] = 'Robo de tienda',
+	['press_to_rob'] = 'Presione ~INPUT_CONTEXT~ para ~o~rob~s~ ~b~%s~s~',
+	['robbery_timer'] = 'Robo en tienda: ~r~%s~s~ segundos restantes',
+	['recent_robbed'] = 'Esta tienda fue robada recientemente. Espere ~y~%s~s~ segundos hasta que pueda volver a robar ',
+	['rob_in_prog'] = '~r~Robo en curso en ~b~%s~s~',
+	['started_to_rob'] = 'Empezaste a robar ~y~%s~s~',
+	['alarm_triggered'] = 'La alarma ha sido activada',
+	['robbery_complete'] = '~r~El robo ha tenido éxito ~s~, usted ~o~robó~s~ ~g~$%s~s~',
+	['robbery_complete_at'] = '~r~ Robo exitoso en ~y~%s~s~',
+	['robbery_cancelled'] = '¡El robo ha sido cancelado!',
+	['robbery_cancelled_at'] = '~r~ ¡El robo en ~b~%s~s~ ha sido cancelado!',
+	['min_police'] = 'Debe haber al menos ~b~%s policías~s~ en la ciudad para robar.',
+	['robbery_already'] = '~r~ Ya se está produciendo un robo.',
+	['no_threat'] = 'No representas una amenaza para el dueño de la tienda',
+}
+
+
+
