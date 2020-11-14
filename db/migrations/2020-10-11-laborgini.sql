53ac9ed3c3938442c1f71cbf0cf657d433de48b2
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-11-14 08:05:13 -0500

    cambios para nuevos autos

--- db/migrations/2020-10-11-laborgini.sql
+++ db/migrations/2020-10-11-laborgini.sql
@@ -1,4 +1,26 @@
 INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2014 Lamborghini Huracan LP610-4', 'lp610', '390000', 'reales');
 INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2019 Lamborghini SC18 Alston', 'sc18', '7000000', 'reales');
+
 INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Caprice Ls 2013', 'caprice13', '25000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Ferrari GTC4 Lusso', 'GTCL', '300000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Ferrari GTO 599XX', '599gtox', '1750000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Lexus GS F 2019', 'gsf', '85000', 'reales');
+
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Nissan Armada Nismo', 'tulenis', '65000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Nissan GT R Nismo 2020', 'nismo20', '211000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Nissan Patrol Super Safari 2016', 'patrolsafari', '47000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Chevrolet Corvette ZR1', 'corvetteZR1', '120000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Chevrolet Camaro ZL1 2018', '2018zl1', '70000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Chevrolet Suburban 2016', 'SUBN', '66000', 'reales');
+
+
+
+
+
+
+
+
+
+
+
 
