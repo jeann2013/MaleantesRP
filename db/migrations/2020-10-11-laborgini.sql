b525b359cc0e9b4741fd770d94641d8cfee75ed9
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2020-11-14 07:08:46 -0500

    se agregaron autos de policia y un auto real

--- db/migrations/2020-10-11-laborgini.sql
+++ db/migrations/2020-10-11-laborgini.sql
@@ -1,2 +1,4 @@
 INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2014 Lamborghini Huracan LP610-4', 'lp610', '390000', 'reales');
 INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('2019 Lamborghini SC18 Alston', 'sc18', '7000000', 'reales');
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES ('Caprice Ls 2013', 'caprice13', '25000', 'reales');
+
